0e84942d51a25717f1239ccf7ad5e0fe86f7b140
Melanie Couronne <melanie.couronne@gmail.com>
2022-03-01 14:48:39 +0100

    Added validations to story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,4 @@
 class Story < ApplicationRecord
+  validates :universe_max_age, :universe_min_age, :universe_name, :description, :title, :cloudinary_key, presence: true
+  validates :universe_max_age, :universe_min_age, numericality: { only_integer: true, greater_than: 7, less_than: 14 }
 end
