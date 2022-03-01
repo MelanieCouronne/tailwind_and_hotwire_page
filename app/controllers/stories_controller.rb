65e568c841245ba4c8a9c4d5d21dca4431a8279e
Melanie Couronne <melanie.couronne@gmail.com>
2022-03-01 19:31:38 +0100

    Changed display_univers method

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -13,8 +13,8 @@ class StoriesController < ApplicationController
   def display_univers
     @selected_la_foret_enchantee = @stories.select { |story| story.universe_name == 'La forêt enchantée' }
     @selected_la_chaumière_perdue = @stories.select { |story| story.universe_name == 'La chaumière perdue' }
-    @selected_ile_mysterieuse = @stories.select { |story| story.universe_name == 'La chaumière perdue' }
-    @selected_le_palais_merveilleux = @stories.select { |story| story.universe_name == 'La chaumière perdue' }
+    @selected_ile_mysterieuse = @stories.select { |story| story.universe_name == "L'île mystérieuse" }
+    @selected_le_palais_merveilleux = @stories.select { |story| story.universe_name == 'Le palais merveilleux' }
     @selected_le_desert_des_mirages = @stories.select { |story| story.universe_name == 'Le désert des mirages' }
   end
 end
